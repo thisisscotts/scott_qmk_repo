dc108af384b6daa41a62f39629d096948811fb54
thisisscotts <this.is.scotts.email.address@gmail.com>
2025-04-07 16:55:25 +0800

    Modified holding term to 150ms
    Moved config from keyboard level to user level
    
    Minor tweaks and clean up.

--- keyboards/keychron/v2/ansi_encoder/config.h
+++ keyboards/keychron/v2/ansi_encoder/config.h
@@ -21,10 +21,3 @@
 
 /* Enable caps-lock LED */
 #define CAPS_LOCK_LED_INDEX 30
-
-/* Combo mods */
-#define EXTRA_SHORT_COMBOS          // maybe save memory
-#define COMBO_TERM 20               // how quickly all combo keys must be pressed in succession to trigger
-#define COMBO_MUST_HOLD_MODS        // if a combo triggers a modifier, only trigger when the combo is held
-#define COMBO_HOLD_TERM 100         // how long at least one of the combo keys must be held to trigger
-#define COMBO_ONLY_FROM_LAYER 0     // combo keys are always checked from layer 0, even if other layers are active.
